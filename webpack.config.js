df0b790fec98ed0524995d977d10a9e7288428c8
Daniel Garcia <dg2@bellefield.com>
2018-12-09 17:43:41 +0100

    cosas varias

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
         rules: [
             {
                 test: /\.ts$/,
-                use: "ts-loader"
+                use: "babel-loader"
             }
         ]
     }
